03cf53f759ca597b8138453d8a6678f0046d481d
Andy <andybrummittps4@hotmail.com>
2022-07-26 17:28:19 +0100

    eslint build setting and removed unused isHome function

--- components/isHome.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-
-const isHome = () => {
-    const router = useRouter();
-    return router.route === "/";
-}
-
-export default isHome
